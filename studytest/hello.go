535f11e1e2b5a2699a9dac8735d8330fdfcf319d
xuxuclassmate <xuxuclassmate@gmail.com>
2023-04-10 13:19:39 +0800

    studytest

---
+++ studytest/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//print:
+	fmt.Println("Hello, World!")
+	fmt.Println("菜鸟旭旭，开始学Go语言")
+
+	//print
+	var a string = "旭旭是菜鸟"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	f := "XuXuClassMate"
+	var d string = "xuxuclassmate,hh"
+
+	fmt.Println(f, d)
+}
